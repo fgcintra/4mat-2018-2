6f706559238322c065ad5f29739b4e50e17d0039
fgcintra <fausto@fgcintra.pro.br>
2018-08-29 10:59:05 -0300

    (29/08) Recuperação, alteração e exclusão de objetos com Mongoose

--- back-end/routes/item.js
+++ back-end/routes/item.js
@@ -3,5 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 router.put('/', controller().novo);
+router.get('/', controller().listar);
+router.get('/:id', controller().obterUm);
+router.patch('/', controller().atualizar);
+router.delete('/:id', controller().excluir);
 
 module.exports = router;
